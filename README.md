71702a8e82a048d66e2da1323edd4ede753b2ca5
rosik <rosik@student.wszib.edu.pl>
2023-10-08 16:22:05 +0200

    Dodano komentarze w pliku AddingTwoNumbers i grafikę w pliku README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # podyplomowe
 Program dodaje dwie liczby całkowite.
+
+![Symbol dodawania](https://www.symbole.pl/wp-content/uploads/2020/09/dodawanie.jpg)
--- src/AddingTwoNumbers.java
+++ src/AddingTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddingTwoNumbers {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 17;
+        int a = 5; //Pierwsza liczba
+        int b = 17; //Druga liczba
         System.out.println("Suma liczb " + a + " i " + b + " jest równa " + (a+b) + ".");
     }
 }
