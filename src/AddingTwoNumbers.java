b2d048cc00afdb07a526012e57798a993063ac3f
rosik <rosik@student.wszib.edu.pl>
2023-10-08 16:07:48 +0200

    Initial commit

---
+++ src/AddingTwoNumbers.java
@@ -0,0 +1,7 @@
+public class AddingTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 17;
+        System.out.println("Suma liczb " + a + " i " + b + " jest równa " + (a+b) + ".");
+    }
+}
